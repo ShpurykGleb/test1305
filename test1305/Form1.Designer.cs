e1bf6e14089069be5aa8949db58f81c2bcbf51cb
ShpurykGleb <110291304+ShpurykGleb@users.noreply.github.com>
2023-05-13 11:00:39 +0300

    Добавьте файлы проекта.

---
+++ test1305/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace test1305
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelConfirmPassword = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelBitrthdayDate = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxConfirmPassword = new System.Windows.Forms.TextBox();
+            this.textBoxBirthdayDate = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.buttonLocalization = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            resources.ApplyResources(this.labelLogin, "labelLogin");
+            this.labelLogin.Name = "labelLogin";
+            // 
+            // labelPassword
+            // 
+            resources.ApplyResources(this.labelPassword, "labelPassword");
+            this.labelPassword.Name = "labelPassword";
+            // 
+            // labelConfirmPassword
+            // 
+            resources.ApplyResources(this.labelConfirmPassword, "labelConfirmPassword");
+            this.labelConfirmPassword.Name = "labelConfirmPassword";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // labelBitrthdayDate
+            // 
+            resources.ApplyResources(this.labelBitrthdayDate, "labelBitrthdayDate");
+            this.labelBitrthdayDate.Name = "labelBitrthdayDate";
+            // 
+            // labelEmail
+            // 
+            resources.ApplyResources(this.labelEmail, "labelEmail");
+            this.labelEmail.Name = "labelEmail";
+            // 
+            // textBoxLogin
+            // 
+            resources.ApplyResources(this.textBoxLogin, "textBoxLogin");
+            this.textBoxLogin.Name = "textBoxLogin";
+            // 
+            // textBoxPassword
+            // 
+            resources.ApplyResources(this.textBoxPassword, "textBoxPassword");
+            this.textBoxPassword.Name = "textBoxPassword";
+            // 
+            // textBoxConfirmPassword
+            // 
+            resources.ApplyResources(this.textBoxConfirmPassword, "textBoxConfirmPassword");
+            this.textBoxConfirmPassword.Name = "textBoxConfirmPassword";
+            // 
+            // textBoxBirthdayDate
+            // 
+            resources.ApplyResources(this.textBoxBirthdayDate, "textBoxBirthdayDate");
+            this.textBoxBirthdayDate.Name = "textBoxBirthdayDate";
+            // 
+            // textBoxEmail
+            // 
+            resources.ApplyResources(this.textBoxEmail, "textBoxEmail");
+            this.textBoxEmail.Name = "textBoxEmail";
+            // 
+            // buttonLocalization
+            // 
+            resources.ApplyResources(this.buttonLocalization, "buttonLocalization");
+            this.buttonLocalization.Name = "buttonLocalization";
+            this.buttonLocalization.UseVisualStyleBackColor = true;
+            this.buttonLocalization.Click += new System.EventHandler(this.buttonLocalization_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.buttonLocalization);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxBirthdayDate);
+            this.Controls.Add(this.textBoxConfirmPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelBitrthdayDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelConfirmPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelConfirmPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelBitrthdayDate;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxConfirmPassword;
+        private System.Windows.Forms.TextBox textBoxBirthdayDate;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button buttonLocalization;
+    }
+}
+
---
+++ test1305/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1305
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLocalization_Click(object sender, EventArgs e)
+        {
+            switch (buttonLocalization.Text)
+            {
+                case "ENG":
+                    {
+                        ChangeLocalization("ru-RU");
+
+                        break;
+                    }
+                case "RU":
+                    {
+                        ChangeLocalization("uk-UA");
+                        break;
+                    }
+                case "UA":
+                    {
+                        ChangeLocalization("en-EN");
+                        break;
+                    }
+            }
+        }
+
+        private void ChangeLocalization(string lang)
+        {
+            ComponentResourceManager componentResourceManager = new ComponentResourceManager(typeof(Form1));
+            CultureInfo cultureInfo = new CultureInfo(lang);
+
+            foreach (Control item in this.Controls)
+            {
+                componentResourceManager.ApplyResources(item, item.Name, cultureInfo);
+            }
+        }
+    }
+}
